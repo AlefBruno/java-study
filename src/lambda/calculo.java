f9d3f05673817d6252700c3f0876dc7475a4e1fd
Alef Rodrigues <alefbruno@Outlook.com>
2022-10-18 14:31:38 -0300

    Incluindo estudos de lambda, BinaryOperator

--- src/lambda/calculo.java
+++ src/lambda/calculo.java
@@ -1,5 +1,8 @@
 package lambda;
 
+
+// @FuncionalInterface é uma regra na qual so podemos ter uma unica função/método na classe.
+@FunctionalInterface
 public interface calculo {
     public abstract double executar(double a, double b);
 }
